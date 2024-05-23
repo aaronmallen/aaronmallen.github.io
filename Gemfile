8dea997d03fdb9d96f6db29580d99f9f467dedef
Aaron Allen <hello@aaronmallen.me>
2024-05-23 18:02:07 -0500

    v2 site

--- Gemfile
+++ Gemfile
@@ -2,23 +2,20 @@
 
 source 'https://rubygems.org'
 
-gem 'http_parser.rb', '~> 0.6.0', platforms: [:jruby]
-gem 'jekyll', '~> 4.3.2'
-gem 'wdm', '~> 0.1.1', platforms: %i[mingw x64_mingw mswin]
+gem 'csv'
+gem 'jekyll'
 
 group :jekyll_plugins do
-  gem 'jekyll-feed', '~> 0.12'
-  gem 'jekyll-inline-svg', '~> 1.1'
-end
-
-platforms :mingw, :x64_mingw, :mswin, :jruby do
-  gem 'tzinfo', '>= 1', '< 3'
-  gem 'tzinfo-data'
+  gem 'jekyll-feed'
+  gem 'jekyll-inline-svg'
+  gem 'jekyll-minifier'
+  gem 'jekyll-sitemap'
 end
 
 group :development, :test do
-  gem 'code-scanning-rubocop', '~> 0.6'
-  gem 'rubocop', '>= 1.45.0', '< 2'
-  gem 'rubocop-jekyll', '~> 0.13'
-  gem 'rubocop-performance', '~> 1.19'
+  gem 'base64'
+  gem 'code-scanning-rubocop'
+  gem 'rubocop'
+  gem 'rubocop-jekyll'
+  gem 'rubocop-performance'
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,17 +1,40 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: [
-    './_drafts/**/*.html',
-    './_includes/**/*.html',
-    './_layouts/**/*.html',
-    './_posts/*.md',
-    './*.md',
-    './*.html',
-  ],
+  mode: 'jti',
+  content: ['./**/*.{html,md}'],
   theme: {
-    theme: {
-      extend: {},
+    extend: {
+      colors: {
+        'daisy-bush': {
+          DEFAULT: '#451ea1',
+          50: '#f4f3ff',
+          100: '#ebe9fe',
+          200: '#d9d6fe',
+          300: '#bdb4fe',
+          400: '#9c8afb',
+          500: '#7b5bf7',
+          600: '#6b38ef',
+          700: '#5c26db',
+          800: '#4d20b7',
+          900: '#451ea1',
+          950: '#260f66',
+        },
+      },
+      fontFamily: {
+        poppins: [
+          'Poppins',
+          '-apple-system',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica Neue',
+          'Arial',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+        ],
+      },
     },
   },
-  plugins: []
+  plugins: [require('@tailwindcss/typography')],
 }
