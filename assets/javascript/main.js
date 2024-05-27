1cc6ce672dc7ad11ca3b7f51dc19dfb649fe7b0e
Aaron Allen <hello@aaronmallen.me>
2024-05-27 09:48:17 -0500

    collapse navbar on link click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,11 +4,22 @@ function initializeMainNavigationCollapseListener() {
 
   if (!toggle || !target) return
 
+  const links = target.querySelectorAll('a')
+
+  const collapseNavbar = () => {
+    target.classList.add('hidden')
+    toggle.setAttribute('aria-expanded', 'false')
+  }
+
   toggle.addEventListener('click', () => {
     const expanded = toggle.getAttribute('aria-expanded') === 'true' || false
     console.log(expanded)
     target.classList.toggle('hidden', expanded)
     toggle.setAttribute('aria-expanded', !expanded)
+  })
+
+  links.forEach(link => {
+    link.addEventListener('click', collapseNavbar)
   })
 }
 
